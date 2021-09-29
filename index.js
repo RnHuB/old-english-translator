cfddf1a05bc8f6a868ebf5e2b168d132b1a94efb
rnhub <dayal.rn@gmail.com>
2021-09-29 16:56:57 +0530

    my first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var translateBtn = document.querySelector('#translateBtn');
+var output = document.querySelector('#output');
+
+
+const errorHandler = (error) => {
+
+    alert(`You reached Maximum attempts. Please try after an hours.`)
+    console.log(error)
+}
+
+
+const handleClick = () => {
+    var inputText = document.querySelector('#inputText').value;
+    var apiUrl = `https://api.funtranslations.com/translate/oldenglish.json?text=${inputText}`;
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(json => {
+            output.innerText = json.contents.translated;
+            })
+        .catch((error) => errorHandler(error))
+}
+
+translateBtn.addEventListener('click', handleClick)
